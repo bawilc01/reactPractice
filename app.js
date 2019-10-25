a51adcdf5fd9d4f905ff3e5b7ceb131a4a8d9d50
Brittney <3944682+bawilc01@users.noreply.github.com>
2019-10-25 01:52:28 -0400

    updated to jsx

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const title = <h1>My First Reach Element!</h1>
+const title = <h1>My First React Element!</h1>
 const desc = <p>I just learned how to create a React node and render it into the DOM.</p>
+
+/*const title = React.createElement(
+    'h1',
+    {id: 'main-title', title: 'This is a title'},
+    'My First React Element!'
+);
+
+const desc = React.createElement(
+    'p',
+    null,
+    'I just learned how to create a React node and render it into the DOM.'
+);*/
 
 const header = React.createElement(
     'header',
