e99020adc92b270223b3e7f966735f2529e593e4
Brittney <3944682+bawilc01@users.noreply.github.com>
2019-10-25 02:10:03 -0400

    jsx for title and desc

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
-const title = <h1>My First React Element!</h1>
-const desc = <p>I just learned how to create a React node and render it into the DOM.</p>
-
-/*const title = React.createElement(
-    'h1',
-    {id: 'main-title', title: 'This is a title'},
-    'My First React Element!'
-);
-
-const desc = React.createElement(
-    'p',
-    null,
-    'I just learned how to create a React node and render it into the DOM.'
-);*/
+const title = <h1>First React Element</h1>;
+const desc = <p>I just learn how to create a React node and render it to DOM</p>;
 
 const header = React.createElement(
     'header',
-    null,
-    title, 
-    desc
-);
-
-
+     null,
+     title, 
+     desc
+ );
 ReactDOM.render(
-    header, 
-    document.getElementById('root')
+    header, document.getElementById("root")
 );
